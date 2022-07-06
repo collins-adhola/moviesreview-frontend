560f98227ae1501444fda38287af0c4084f851bf
Collino <coadhola@gmail.com>
2022-07-06 17:05:01 +0100

    Movie added

--- src/components/movies.js
+++ src/components/movies.js
@@ -54,6 +54,29 @@ function Movie() {
             </Card>
             <br></br>
             <h2>Reviews</h2>
+            <br></br>
+            {movie.reviews.map((review, index) => {
+              return (
+                <Media key={index}>
+                  <Media.Body>
+                    <h5>{review.name + " reviewed on " + review.date}</h5>
+                    <p>{review.review}</p>
+                    {props.user && props.user.id === review.user_id && 
+                      <Row>
+                        <Col><Link to {{
+                          pathName: "/movies/"+
+                                  props.match.params.id+
+                                  "/review",
+                          state: {currentReview: review}
+                        }}>Edit</Link>
+                      </Col>
+                      <Col><Button variant="link">Delete</Button></Col>
+                      </Row>
+                    }
+                  </Media.Body>
+                </Media>
+              )
+            })}
           </Col>
         </Row>
       </Container>
